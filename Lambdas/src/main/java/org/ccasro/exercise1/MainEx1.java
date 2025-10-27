c2f72725a565c71fae3182d27b41a7ee5612e364
ccasro <ccasro@gmail.com>
2025-10-27 11:23:35 +0100

    feat(level1): add complete Exercise 1

---
+++ Lambdas/src/main/java/org/ccasro/exercise1/MainEx1.java
@@ -0,0 +1,17 @@
+package org.ccasro.exercise1;
+
+import java.util.List;
+
+public class MainEx1 {
+    public static void main(String[] args){
+        List<String> stringList = List.of("Hello","Flower","example","Clear","Avoid","Important","Seal","Soul"
+        );
+
+        System.out.println(listWithO(stringList));
+
+    }
+
+    private static List<String> listWithO(List<String> list) {
+        return list.stream().filter(s->s.toLowerCase().contains("o")).toList();
+    }
+}
